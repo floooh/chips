9a66607afeddb601c05d224d7f1a1055b01da882
Andre Weissflog <floooh@gmail.com>
2017-12-15 15:46:51 +0100

    new header: clk.h

---
+++ chips/clk.h
@@ -0,0 +1,34 @@
+#pragma once
+/*
+    clk.h -- clock, timer and counter helpers
+*/
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* convert microseconds to number of clock ticks */
+extern int64_t clk_ticks(int freq_khz, int micro_secs);
+
+/*-- IMPLEMENTATION ----------------------------------------------------------*/
+#ifdef CHIPS_IMPL
+#include <string.h>
+#ifndef CHIPS_DEBUG
+    #ifdef _DEBUG
+        #define CHIPS_DEBUG
+    #endif
+#endif
+#ifndef CHIPS_ASSERT
+    #include <assert.h>
+    #define CHIPS_ASSERT(c) assert(c)
+#endif
+
+int64_t clk_ticks(int freq_khz, int micro_secs) {
+    return ((int64_t)(freq_khz * micro_secs)) / 1000;
+}
+#endif
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
