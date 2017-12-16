c2a48982e0ab436fbfbc583f918b05983a447eaf
Andre Weissflog <floooh@gmail.com>
2017-12-16 22:17:27 +0100

    Minor tweaks in beeper.h

--- chips/beeper.h
+++ chips/beeper.h
@@ -12,7 +12,7 @@ extern "C" {
 #endif
 
 /* super-sampling precision */
-#define BEEPER_SUPERSAMPLES (4)
+#define BEEPER_SUPER_SAMPLES (2)
 
 /* beeper state */
 typedef struct {
@@ -33,7 +33,7 @@ typedef struct {
 } beeper_t;
 
 /* initialize beeper instance */
-extern void beeper_init(beeper_t* beeper, int cpu_khz, int sound_hz, float magnitude);
+extern void beeper_init(beeper_t* beeper, int tick_khz, int sound_hz, float magnitude);
 /* set current on/off state */
 static inline void beeper_write(beeper_t* beeper, bool state) {
     beeper->state = state;
@@ -51,8 +51,8 @@ static inline bool beeper_tick(beeper_t* b, int num_ticks) {
             b->acc += b->mag;
         }
         if (--b->super_sample_counter == 0) {
-            b->super_sample_counter = BEEPER_SUPERSAMPLES;
-            b->sample = b->acc / BEEPER_SUPERSAMPLES;
+            b->super_sample_counter = BEEPER_SUPER_SAMPLES;
+            b->sample = b->acc / BEEPER_SUPER_SAMPLES;
             b->acc = 0.0f;
             return true;
         }
@@ -73,18 +73,14 @@ static inline bool beeper_tick(beeper_t* b, int num_ticks) {
     #define CHIPS_ASSERT(c) assert(c)
 #endif
 
-void beeper_init(beeper_t* b, int cpu_khz, int sound_hz, float magnitude) {
+void beeper_init(beeper_t* b, int tick_khz, int sound_hz, float magnitude) {
     CHIPS_ASSERT(b);
-    CHIPS_ASSERT(cpu_khz > 0);
-    CHIPS_ASSERT(sound_hz > 0);
+    CHIPS_ASSERT((tick_khz > 0) && (sound_hz > 0));
     memset(b, 0, sizeof(*b));
-    b->period = (cpu_khz * 1000) / (sound_hz * BEEPER_SUPERSAMPLES);
+    b->period = (tick_khz * 1000) / (sound_hz * BEEPER_SUPER_SAMPLES);
     b->tick_counter = b->period;
-    b->super_sample_counter = BEEPER_SUPERSAMPLES;
-    b->state = false;
+    b->super_sample_counter = BEEPER_SUPER_SAMPLES;
     b->mag = magnitude;
-    b->acc = 0.0f;
-    b->sample = 0.0f;
 }
 
 #endif /* CHIPS_IMPL */
