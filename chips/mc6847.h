ee47b87a6d7949be0b247207873cf809e29d9172
Andre Weissflog <floooh@gmail.com>
2017-12-30 23:22:31 +0100

    started with mc6847

---
+++ chips/mc6847.h
@@ -0,0 +1,15 @@
+#pragma once
+/*
+    mc6847.h -- Motorola 6847 Video Display Generator
+
+    Do this:
+        #define CHIPS_IMPL
+    before you include this file in *one* C or C++ file to create the 
+    implementation.
+
+    Optionally provide the following macros with your own implementation
+    
+        CHIPS_ASSERT(c)     -- your own assert macro (default: assert(c))
+
+
+*/
