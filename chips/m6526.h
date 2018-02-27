ff05c838224dbd9f135c67b5f0d5073d02b96e33
Andre Weissflog <floooh@gmail.com>
2018-02-27 21:54:13 +0100

    more m6526 impl work

--- chips/m6526.h
+++ chips/m6526.h
@@ -36,7 +36,14 @@
     *   DB7 --->|           |          *
     *           +-----------+          *
     ************************************
-    
+
+    ## NOT IMPLEMENTED:
+
+    - handshake (FLAG and PC pin)
+    - time of day clock
+    - serial port
+    - no external counter trigger via CNT pin
+
     TODO: Documentation
     
     ## MIT License
@@ -73,6 +80,7 @@ extern "C" {
 #define M6526_RS1   (1ULL<<1)
 #define M6526_RS2   (1ULL<<2)
 #define M6526_RS3   (1ULL<<3)
+#define M6526_RS    (M6526_RS3|M6526_RS2|M6526_RS1|M6526_RS0)
 
 /* data bus pins shared with CPU */
 #define M6526_D0    (1ULL<<16)
@@ -116,6 +124,118 @@ extern "C" {
 #define M6526_PB6   (1ULL<<62)
 #define M6526_PB7   (1ULL<<63)
 
+/* register indices */
+#define M6526_REG_PRA       (0)     /* peripheral data reg A */
+#define M6526_REG_PRB       (1)     /* peripheral data reg B */
+#define M6526_REG_DDRA      (2)     /* data direction reg A */
+#define M6526_REG_DDRB      (3)     /* data direction reg B */
+#define M6526_REG_TALO      (4)     /* timer A low register */
+#define M6526_REG_TAHI      (5)     /* timer A high register */
+#define M6526_REG_TBLO      (6)     /* timer B low register */
+#define M6526_REG_TBHI      (7)     /* timer B high register */
+#define M6526_REG_TOD10TH   (8)     /* 10ths of seconds register */
+#define M6526_REG_TODSEC    (9)     /* seconds register */
+#define M6526_REG_TODMIN    (10)    /* minutes register */
+#define M6526_REG_TODHR     (11)    /* hours am/pm register */
+#define M6526_REG_SDR       (12)    /* serial data register */
+#define M6526_REG_ICR       (13)    /* interrupt control register */
+#define M6526_REG_CRA       (14)    /* control register A */
+#define M6526_REG_CRB       (15)    /* control register B */
+
+/* control register bits */
+#define M6526_CRA_START                 (1<<0)
+#define M6526_CRA_START_START           (1<<0)  /* start timer A (reset on underflow in oneshot mode) */
+#define M6526_CRA_START_STOP            (0)     /* stop timer A */
+
+#define M6526_CRA_PBON                  (1<<1)
+#define M6526_CRA_PBON_PB6ON            (1<<1)  /* timer A output appears on PB6 */
+#define M6526_CRA_PBON_PB6OFF           (0)     /* PB6 normal operation */
+
+#define M6526_CRA_OUTMODE               (1<<2)
+#define M6526_CRA_OUTMODE_TOGGLE        (1<<2)
+#define M6526_CRA_OUTMODE_PULSE         (0)
+
+#define M6526_CRA_RUNMODE               (1<<3)
+#define M6526_CRA_RUNMODE_ONESHOT       (1<<3)
+#define M6526_CRA_RUNMODE_CONTINUOUS    (0)
+
+#define M6526_CRA_FORCE_LOAD            (1<<4)  /* strobe only, no latch, read will always return 0 */
+
+#define M6526_CRA_INMODE                (1<<5)
+#define M6526_CRA_INMODE_CNT            (1<<5)  /* timer A counts positive CNT pin transitions */
+#define M6526_CRA_INMODE_CLK            (0)     /* timer A counts clock ticks */
+
+#define M6526_CRA_SPMODE                (1<<6)
+#define M6526_CRA_SPMODE_OUTPUT         (1<<6)
+#define M6526_CRA_SPMODE_INPUT          (0)
+
+#define M6526_CRA_TODIN                 (1<<7)
+#define M6526_CRA_TODIN_50HZ            (1<<7)
+#define M6526_CRA_TODIN_60HZ            (0)
+
+#define M6526_CRB_START                 (1<<0)
+#define M6526_CRB_START_START           (1<<0)  /* start timer B (reset on underflow in oneshot mode) */
+#define M6526_CRB_START_STOP            (0)     /* stop timer B */
+
+#define M6526_CRB_PBON                  (1<<1)
+#define M6526_CRB_PBON_PB7ON            (1<<1)  /* timer B output appears on PB7 */
+#define M6526_CRB_PBON_PB7OFF           (0)     /* PB7 normal operation */
+
+#define M6526_CRB_OUTMODE               (1<<2)
+#define M6526_CRB_OUTMODE_TOGGLE        (1<<2)
+#define M6526_CRB_OUTMODE_PULSE         (0)
+
+#define M6526_CRB_RUNMODE               (1<<3)
+#define M6526_CRB_RUNMODE_ONESHOT       (1<<3)
+#define M6526_CRB_RUNMODE_CONTINUOUS    (0)
+
+#define M6526_CRB_FORCE_LOAD            (1<<4)  /* strobe only, no latch, read will always return 0 */
+
+#define M6526_CRB_INMODE                ((1<<6)|(1<<5))
+#define M6526_CRB_INMODE_CLK            (0)     /* timer B counts clock ticks */
+#define M6526_CRB_INMODE_CNT            (1<<5)  /* timer B counts positive CNT pin transitions */
+#define M6526_CRB_INMODE_TA             (1<<6)  /* timer B counts timer A underflow pulses */
+#define M6526_CRB_INMODE_CNTTA          ((1<<6)|(1<<5)) /* timer B counts timer A underflow pulses while CNT is high */
+
+#define M6526_CRB_ALARM                 (1<<7)
+#define M6526_CRB_ALARM_TOD             (1<<7)  /* writing to TOD registers sets alarm */
+#define M6526_CRB_ALARM_ALARM           (0)     /* writing to TOD registers sets TOD clock */
+
+/* port in/out callbacks */
+#define M6526_PORT_A (0)
+#define M6526_PORT_B (1)
+typedef uint8_t (*m6526_in_t)(int port_id);
+typedef void (*m6526_out_t)(int port_id, uint8_t data);
+
+/* m6526 state */
+typedef struct {
+    uint8_t out_a, in_a, ddr_a;
+    uint8_t out_b, in_b, ddr_b;
+    uint16_t ta_latch, tb_latch;
+    uint16_t ta_counter, tb_counter;
+    uint8_t icr, cra, crb;
+    bool ta_active, tb_active;
+    bool ta_pb6, tb_pb7;
+    m6526_in_t in_cb;
+    m6526_out_t out_cb;
+} m6526_t;
+
+/* extract 8-bit data bus from 64-bit pins */
+#define M6526_GET_DATA(p) ((uint8_t)(p>>16))
+/* merge 8-bit data bus value into 64-bit pins */
+#define M6526_SET_DATA(p,d) {p=((p&~0xFF0000)|((d&0xFF)<<16));}
+/* merge 4-bit register-select address into 64-bit pins */
+#define M6526_SET_ADDR(p,d) {p=((p&~0xF)|(d&0xF));}
+
+/* initialize a new m6526_t instance */
+extern void m6526_init(m6526_t* c, m6526_in_t in_cb, m6526_out_t out_cb);
+/* reset an existing m6526_t instance */
+extern void m6526_reset(m6526_t* c);
+/* perform an IO request */
+extern uint64_t m6526_iorq(m6526_t* c, uint64_t pins);
+/* tick the m6526_t instance, this may trigger the IRQ pin */
+extern uint64_t m6526_tick(m6526_t* c, uint64_t pins);
+
 /*-- IMPLEMENTATION ----------------------------------------------------------*/
 #ifdef CHIPS_IMPL
 #include <string.h>
@@ -128,6 +248,53 @@ extern "C" {
     #include <assert.h>
     #define CHIPS_ASSERT(c) assert(c)
 #endif
+
+void m6526_init(m6526_t* c, m6526_in_t in_cb, m6526_out_t out_cb) {
+    CHIPS_ASSERT(c);
+    memset(c, 0, sizeof(*c));
+    c->in_cb = in_cb;
+    c->out_cb = out_cb;
+    c->ta_latch = 0xFFFF;
+    c->tb_latch = 0xFFFF;
+}
+
+void m6526_reset(m6526_t* c) {
+    CHIPS_ASSERT(c);
+    c->out_a = c->in_a = c->ddr_a = 0;
+    c->out_b = c->in_b = c->ddr_b = 0;
+    c->ta_latch = c->tb_latch = 0xFFFF;
+    c->ta_counter = c->tb_counter = 0;
+    c->icr = c->cra = c->crb = 0;
+    c->ta_active = c->tb_active = false;
+    c->ta_pb6 = c->tb_pb7 = false;
+}
+
+static uint8_t _m6526_read(m6526_t* c, uint8_t addr) {
+
+    return 0xFF;
+}
+
+static void _m6526_write(m6526_t* c, uint8_t addr, uint8_t data) {
+
+}
+
+uint64_t m6526_iorq(m6526_t* c, uint64_t pins) {
+    if (pins & M6526_CS) {
+        uint8_t addr = pins & M6526_RS;
+        if (pins & M6526_RW) {
+            /* a read request */
+            uint8_t data = _m6526_read(c, addr);
+            M6526_SET_DATA(pins, data);
+        }
+        else {
+            /* a write request */
+            uint8_t data = M6526_GET_DATA(pins);
+            _m6526_write(c, addr, data);
+        }
+    }
+    return pins;
+}
+
 
 #endif /* CHIPS_IMPL */
 
