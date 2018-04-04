e763b857244232f3265b37bb6dad8de7a7cbfcda
Andre Weissflog <floooh@gmail.com>
2018-04-04 11:53:55 +0200

    beeper: fix the idle-hissing, and make output softer

--- chips/beeper.h
+++ chips/beeper.h
@@ -13,20 +13,20 @@ extern "C" {
 
 /* error-accumulation precision boost */
 #define BEEPER_FIXEDPOINT_SCALE (16)
+/* minimal update rate in Hz before the beeper goes silent */
+#ifndef BEEPER_SILENCE_HZ
+#define BEEPER_SILENCE_HZ (10)
+#endif
 
 /* beeper state */
 typedef struct {
-    /* current on/off state */
-    bool state;
-    /* current active-state accumulator */
-    uint32_t acc;
-    /* sample period in ticks */
+    int state;    /* can be -1, +1 or 0 (if silent) */
+    uint32_t silent_counter;
+    uint32_t ticks_until_silence;
     int period;
-    /* current tick down-counter */
     int counter;
-    /* multiplier to get scaled sample value */
-    float mul;
-    /* latest computed sample */
+    float mag;
+    float goal;
     float sample;
 } beeper_t;
 
@@ -35,23 +35,36 @@ extern void beeper_init(beeper_t* beeper, int tick_hz, int sound_hz, float magni
 /* reset the beeper instance */
 extern void beeper_reset(beeper_t* beeper);
 /* set current on/off state */
-static inline void beeper_write(beeper_t* beeper, bool state) {
-    beeper->state = state;
+static inline void beeper_set(beeper_t* beeper, bool state) {
+    int new_state = state ? 1 : -1;
+    if (beeper->state != new_state) {
+        beeper->silent_counter = 0;
+        beeper->state = new_state;
+    }
 }
 /* toggle current state (on->off or off->on) */
 static inline void beeper_toggle(beeper_t* beeper) {
-    beeper->state = !beeper->state;
+    if (beeper->state == 0) {
+        beeper->state = 1;
+    }
+    else {
+        beeper->state = -beeper->state;
+    }
+    beeper->silent_counter = 0;
 }
 /* tick the beeper, return true if a new sample is ready */
-static inline bool beeper_tick(beeper_t* b) {
-    if (b->state) {
-        b->acc++;
+static inline bool beeper_tick(beeper_t* beeper) {
+    if (beeper->state != 0) {
+        if (beeper->silent_counter++ > beeper->ticks_until_silence) {
+            beeper->state = 0;
+        }
     }
-    b->counter -= BEEPER_FIXEDPOINT_SCALE;
-    if (b->counter <= 0) {
-        b->counter += b->period;
-        b->sample = ((float)b->acc) * b->mul;
-        b->acc = 0;
+    /* generate a new sample? */
+    beeper->counter -= BEEPER_FIXEDPOINT_SCALE;
+    if (beeper->counter <= 0) {
+        beeper->counter += beeper->period;
+        beeper->goal = ((float)beeper->state) * beeper->mag;
+        beeper->sample += (beeper->goal - beeper->sample) * 0.5f;
         return true;
     }
     return false;
@@ -76,15 +89,16 @@ void beeper_init(beeper_t* b, int tick_hz, int sound_hz, float magnitude) {
     memset(b, 0, sizeof(*b));
     b->period = (tick_hz * BEEPER_FIXEDPOINT_SCALE) / sound_hz;
     b->counter = b->period;
-    b->mul = magnitude * (1.0f / ((float)tick_hz / (float)sound_hz));
+    b->mag = magnitude;
+    b->ticks_until_silence = tick_hz / BEEPER_SILENCE_HZ;
 }
 
 void beeper_reset(beeper_t* b) {
     CHIPS_ASSERT(b);
-    b->acc = 0;
-    b->sample = 0;
-    b->state = false;
+    b->state = 0;
+    b->silent_counter = 0;
     b->counter = b->period;
+    b->sample = 0;
 }
 
 #endif /* CHIPS_IMPL */
