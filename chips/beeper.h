1b7368b0e3babad0944ce91e4b72b0e5b23cf6b5
Andre Weissflog <floooh@gmail.com>
2018-07-20 21:20:05 +0200

    beeper: remove 'silent counter', this created artefacts on ZX Spectrum Boulderdash

--- chips/beeper.h
+++ chips/beeper.h
@@ -13,16 +13,10 @@ extern "C" {
 
 /* error-accumulation precision boost */
 #define BEEPER_FIXEDPOINT_SCALE (16)
-/* minimal update rate in Hz before the beeper goes silent */
-#ifndef BEEPER_SILENCE_HZ
-#define BEEPER_SILENCE_HZ (100)
-#endif
 
 /* beeper state */
 typedef struct {
-    int state;    /* can be -1, +1 or 0 (if silent) */
-    uint32_t silent_counter;
-    uint32_t ticks_until_silence;
+    int state;
     int period;
     int counter;
     float mag;
@@ -35,29 +29,14 @@ extern void beeper_init(beeper_t* beeper, int tick_hz, int sound_hz, float magni
 extern void beeper_reset(beeper_t* beeper);
 /* set current on/off state */
 static inline void beeper_set(beeper_t* beeper, bool state) {
-    int new_state = state ? 1 : -1;
-    if (beeper->state != new_state) {
-        beeper->silent_counter = 0;
-        beeper->state = new_state;
-    }
+    beeper->state = state ? 1 : 0;
 }
 /* toggle current state (on->off or off->on) */
 static inline void beeper_toggle(beeper_t* beeper) {
-    if (beeper->state == 0) {
-        beeper->state = 1;
-    }
-    else {
-        beeper->state = -beeper->state;
-    }
-    beeper->silent_counter = 0;
+    beeper->state = !beeper->state;
 }
 /* tick the beeper, return true if a new sample is ready */
 static inline bool beeper_tick(beeper_t* beeper) {
-    if (beeper->state != 0) {
-        if (beeper->silent_counter++ > beeper->ticks_until_silence) {
-            beeper->state = 0;
-        }
-    }
     /* generate a new sample? */
     beeper->counter -= BEEPER_FIXEDPOINT_SCALE;
     if (beeper->counter <= 0) {
@@ -88,13 +67,11 @@ void beeper_init(beeper_t* b, int tick_hz, int sound_hz, float magnitude) {
     b->period = (tick_hz * BEEPER_FIXEDPOINT_SCALE) / sound_hz;
     b->counter = b->period;
     b->mag = magnitude;
-    b->ticks_until_silence = tick_hz / BEEPER_SILENCE_HZ;
 }
 
 void beeper_reset(beeper_t* b) {
     CHIPS_ASSERT(b);
     b->state = 0;
-    b->silent_counter = 0;
     b->counter = b->period;
     b->sample = 0;
 }
