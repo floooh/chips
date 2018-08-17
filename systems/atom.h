8c7add2fe48208e9ab874b78480a52416b1081d2
Andre Weissflog <floooh@gmail.com>
2018-08-17 19:49:30 +0200

    more atom.h impl

--- systems/atom.h
+++ systems/atom.h
@@ -111,6 +111,8 @@ typedef struct {
     mc6847_t vdg;
     i8255_t ppi;
     m6522_t via;
+    beeper_t beeper;
+    bool valid;
     int counter_2_4khz;
     int period_2_4khz;
     bool state_2_4khz;
@@ -122,10 +124,9 @@ typedef struct {
     clk_t clk;
     mem_t mem;
     kbd_t kbd;
-    uint32_t pixel_buffer;
-    const void* rom_abasic;
-    const void* rom_afloat;
-    const void* rom_dosrom;
+    const uint8_t* rom_abasic;
+    const uint8_t* rom_afloat;
+    const uint8_t* rom_dosrom;
     atom_audio_callback_t audio_cb;
     int num_samples;
     int sample_pos;
@@ -177,5 +178,177 @@ static uint8_t _atom_ppi_in(int port_id, void* user_data);
 static uint64_t _atom_ppi_out(int port_id, uint64_t pins, uint8_t data, void* user_data);
 static uint8_t _atom_via_in(int port_id, void* user_data);
 static void _atom_via_out(int port_id, uint8_t data, void* user_data);
+static void _atom_init_keymap(atom_t* sys);
+static void _atom_init_memorymap(atom_t* sys);
+
+#define _ATOM_DEFAULT(val,def) (((val) != 0) ? (val) : (def));
+
+void atom_init(atom_t* sys, const atom_desc_t* desc) {
+    CHIPS_ASSERT(sys && desc);
+    CHIPS_ASSERT(desc->pixel_buffer && (desc->pixel_buffer_size >= _ATOM_DISPLAY_SIZE));
+    CHIPS_ASSERT(desc->rom_abasic && (desc->rom_abasic_size == _ATOM_ROM_ABASIC_SIZE));
+    CHIPS_ASSERT(desc->rom_afloat && (desc->rom_afloat_size == _ATOM_ROM_AFLOAT_SIZE));
+    CHIPS_ASSERT(desc->rom_dosrom && (desc->rom_dosrom_size == _ATOM_ROM_DOSROM_SIZE));
+
+    memset(sys, 0, sizeof(atom_t));
+    sys->valid = true;
+    sys->audio_cb = desc->audio_cb;
+    sys->num_samples = _ATOM_DEFAULT(desc->audio_num_samples, ATOM_DEFAULT_AUDIO_SAMPLES);
+    CHIPS_ASSERT(sys->num_samples <= ATOM_MAX_AUDIO_SAMPLES);
+    sys->rom_abasic = (const uint8_t*) desc->rom_abasic;
+    sys->rom_afloat = (const uint8_t*) desc->rom_afloat;
+    sys->rom_dosrom = (const uint8_t*) desc->rom_dosrom;
+
+    /* initialize the hardware */
+    clk_init(&sys->clk, _ATOM_FREQUENCY);
+    sys->period_2_4khz = _ATOM_FREQUENCY / 4800;
+
+    m6502_desc_t cpu_desc = {0};
+    cpu_desc.tick_cb = _atom_tick;
+    cpu_desc.user_data = sys;
+    m6502_init(&sys->cpu, &cpu_desc);
+
+    mc6847_desc_t vdg_desc = {0};
+    vdg_desc.tick_hz = _ATOM_FREQUENCY;
+    vdg_desc.rgba8_buffer = desc->pixel_buffer;
+    vdg_desc.rgba8_buffer_size = desc->pixel_buffer_size;
+    vdg_desc.fetch_cb = _atom_vdg_fetch;
+    vdg_desc.user_data = sys;
+    mc6847_init(&sys->vdg, &vdg_desc);
+
+    i8255_desc_t ppi_desc = {0};
+    ppi_desc.in_cb = _atom_ppi_in;
+    ppi_desc.out_cb = _atom_ppi_out;
+    ppi_desc.user_data = sys;
+    i8255_init(&sys->ppi, &ppi_desc);
+
+    m6522_desc_t via_desc = {0};
+    via_desc.in_cb = _atom_via_in;
+    via_desc.out_cb = _atom_via_out;
+    via_desc.user_data = sys;
+    m6522_init(&sys->via, &via_desc);
+
+    const int audio_hz = _ATOM_DEFAULT(desc->audio_sample_rate, 44100);
+    const float audio_vol = _ATOM_DEFAULT(desc->audio_volume, 0.5f);
+    beeper_init(&sys->beeper, _ATOM_FREQUENCY, audio_hz, audio_vol);
+
+    /* setup memory map and keyboard matrix */
+    _atom_init_memorymap(sys);
+    _atom_init_keymap(sys);
+
+    /* trap the OSLOAD function (http://ladybug.xs4all.nl/arlet/fpga/6502/kernel.dis) */
+    m6502_set_trap(&sys->cpu, 1, 0xF96E);
+
+    /* CPU start state */
+    m6502_reset(&sys->cpu);
+}
+
+void atom_discard(atom_t* sys) {
+    CHIPS_ASSERT(sys && sys->valid);
+    sys->valid = false;
+}
+
+void atom_reset(atom_t* sys) {
+    CHIPS_ASSERT(sys && sys->valid);
+    m6502_reset(&sys->cpu);
+    i8255_reset(&sys->ppi);
+    m6522_reset(&sys->via);
+    mc6847_reset(&sys->vdg);
+    beeper_reset(&sys->beeper);
+    sys->state_2_4khz = false;
+    sys->out_cass0 = false;
+    sys->out_cass1 = false;
+}
+
+void atom_exec(atom_t* sys, double seconds) {
+    CHIPS_ASSERT(sys && sys->valid);
+    uint32_t ticks_to_run = clk_ticks_to_run(&sys->clk, seconds);
+    uint32_t ticks_executed = m6502_exec(&sys->cpu, ticks_to_run);
+    clk_ticks_executed(&sys->clk, ticks_executed);
+    kbd_update(&sys->kbd);
+}
+
+static void _atom_via_out(int port_id, uint8_t data, void* user_data) {
+    /* FIXME */
+}
+
+static uint8_t _atom_via_in(int port_id, void* user_data) {
+    /* FIXME */
+    return 0x00;
+}
+
+static void _atom_init_keymap(atom_t* sys) {
+    /*  setup the keyboard matrix
+        the Atom has a 10x8 keyboard matrix, where the
+        entire line 6 is for the Ctrl key, and the entire
+        line 7 is the Shift key
+    */
+    kbd_init(&sys->kbd, 1);
+    /* shift key is entire line 7 */
+    const int shift = (1<<0); kbd_register_modifier_line(&sys->kbd, 0, 7);
+    /* ctrl key is entire line 6 */
+    const int ctrl = (1<<1); kbd_register_modifier_line(&sys->kbd, 1, 6);
+    /* alpha-numeric keys */
+    const char* keymap = 
+        /* no shift */
+        "     ^]\\[ "/**/"3210      "/* */"-,;:987654"/**/"GFEDCBA@/."/**/"QPONMLKJIH"/**/" ZYXWVUTSR"
+        /* shift */
+        "          "/* */"#\"!       "/**/"=<+*)('&%$"/**/"gfedcba ?>"/**/"qponmlkjih"/**/" zyxwvutsr";
+    for (int layer = 0; layer < 2; layer++) {
+        for (int column = 0; column < 10; column++) {
+            for (int line = 0; line < 6; line++) {
+                int c = keymap[layer*60 + line*10 + column];
+                if (c != 0x20) {
+                    kbd_register_key(&sys->kbd, c, column, line, layer?shift:0);
+                }
+            }
+        }
+    }
+    /* special keys */
+    kbd_register_key(&sys->kbd, 0x20, 9, 0, 0);         /* space */
+    kbd_register_key(&sys->kbd, 0x01, 4, 1, 0);         /* backspace */
+    kbd_register_key(&sys->kbd, 0x07, 0, 3, ctrl);      /* Ctrl+G: bleep */
+    kbd_register_key(&sys->kbd, 0x08, 3, 0, shift);     /* key left */
+    kbd_register_key(&sys->kbd, 0x09, 3, 0, 0);         /* key right */
+    kbd_register_key(&sys->kbd, 0x0A, 2, 0, shift);     /* key down */
+    kbd_register_key(&sys->kbd, 0x0B, 2, 0, 0);         /* key up */
+    kbd_register_key(&sys->kbd, 0x0D, 6, 1, 0);         /* return/enter */
+    kbd_register_key(&sys->kbd, 0x0C, 5, 4, ctrl);      /* Ctrl+L clear screen */
+    kbd_register_key(&sys->kbd, 0x0E, 3, 4, ctrl);      /* Ctrl+N page mode on */
+    kbd_register_key(&sys->kbd, 0x0F, 2, 4, ctrl);      /* Ctrl+O page mode off */
+    kbd_register_key(&sys->kbd, 0x15, 6, 5, ctrl);      /* Ctrl+U end screen */
+    kbd_register_key(&sys->kbd, 0x18, 3, 5, ctrl);      /* Ctrl+X cancel */
+    kbd_register_key(&sys->kbd, 0x1B, 0, 5, 0);         /* escape */
+}
+
+static uint32_t _atom_xorshift32(uint32_t x) {
+    x ^= x<<13;
+    x ^= x>>17;
+    x ^= x<<5;
+    return x;
+}
+
+static void _atom_init_memorymap(atom_t* sys) {
+    mem_init(&sys->mem);
+
+    /* fill memory with random junk */
+    uint32_t r = 0x6D98302B;
+    for (int i = 0; i < (int)sizeof(sys->ram);) {
+        r = _atom_xorshift32(r);
+        sys->ram[i++] = r;
+        sys->ram[i++] = (r>>8);
+        sys->ram[i++] = (r>>16);
+        sys->ram[i++] = (r>>24);
+    }
+    /* 32 KB RAM (with RAM extension) + 8 KB vidmem */
+    mem_map_ram(&sys->mem, 0, 0x0000, 0xA000, sys->ram);
+    /* hole in 0xA000 to 0xAFFF (for utility ROMs) */
+    /* 0xB000 to 0xBFFF: IO area, not mapped */
+    /* 16 KB ROMs from 0xC000 */
+    mem_map_rom(&sys->mem, 0, 0xC000, 0x1000, sys->rom_abasic);
+    mem_map_rom(&sys->mem, 0, 0xD000, 0x1000, sys->rom_afloat);
+    mem_map_rom(&sys->mem, 0, 0xE000, 0x1000, sys->rom_dosrom);
+    mem_map_rom(&sys->mem, 0, 0xF000, 0x1000, sys->rom_abasic + 0x1000);
+}
 
 #endif /* CHIPS_IMPL */
