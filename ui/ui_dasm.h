bd950ae37e8bd937115e704f67ebdc7bf1b33e04
Andre Weissflog <floooh@gmail.com>
2018-11-15 12:19:51 +0100

    rename ui_z80dasm to ui_dasm

--- ui/ui_dasm.h
+++ ui/ui_dasm.h
@@ -1,8 +1,8 @@
 #pragma once
 /*#
-    # ui_z80dasm.h
+    # ui_dasm.h
 
-    Z80 disassembler UI using Dear ImGui.
+    Disassembler UI using Dear ImGui.
 
     Do this:
     ~~~C
@@ -25,8 +25,8 @@
         - ui_util.h
         - z80dasm.h
 
-    All strings provided to ui_z80dasm_init() must remain alive until
-    ui_z80dasm_discard() is called!
+    All strings provided to ui_dasm_init() must remain alive until
+    ui_dasm_discard() is called!
 
     ## zlib/libpng license
 
@@ -53,31 +53,31 @@
 extern "C" {
 #endif
 
-#define UI_Z80DASM_MAX_LAYERS (8)
-#define UI_Z80DASM_MAX_STRLEN (32)
-#define UI_Z80DASM_NUM_LINES (256)
+#define UI_DASM_MAX_LAYERS (8)
+#define UI_DASM_MAX_STRLEN (32)
+#define UI_DASM_NUM_LINES (256)
 
 /* callback for reading a byte from memory */
-typedef uint8_t (*ui_z80dasm_read_t)(int layer, uint16_t addr, void* user_data);
+typedef uint8_t (*ui_dasm_read_t)(int layer, uint16_t addr, void* user_data);
 
-/* setup parameters for ui_z80dasm_init()
+/* setup parameters for ui_dasm_init()
 
     NOTE: all strings must be static!
 */
 typedef struct {
     const char* title;  /* window title */
-    const char* layers[UI_Z80DASM_MAX_LAYERS];   /* memory system layer names */
-    ui_z80dasm_read_t read_cb;
+    const char* layers[UI_DASM_MAX_LAYERS];   /* memory system layer names */
+    ui_dasm_read_t read_cb;
     void* user_data;
     int x, y, w, h;     /* initial window pos and size */
-} ui_z80dasm_desc_t;
+} ui_dasm_desc_t;
 
 typedef struct {
     const char* title;
-    ui_z80dasm_read_t read_cb;
+    ui_dasm_read_t read_cb;
     int cur_layer;
     int num_layers;
-    const char* layers[UI_Z80DASM_MAX_LAYERS];
+    const char* layers[UI_DASM_MAX_LAYERS];
     void* user_data;
     int init_x, init_y;
     int init_w, init_h;
@@ -86,23 +86,23 @@ typedef struct {
     uint16_t start_addr;
     uint16_t cur_addr;
     int str_pos;
-    char str_buf[UI_Z80DASM_MAX_STRLEN];
-} ui_z80dasm_t;
+    char str_buf[UI_DASM_MAX_STRLEN];
+} ui_dasm_t;
 
 /* initialize a new window */
-void ui_z80dasm_init(ui_z80dasm_t* win, ui_z80dasm_desc_t* desc);
+void ui_dasm_init(ui_dasm_t* win, ui_dasm_desc_t* desc);
 /* discard a window */
-void ui_z80dasm_discard(ui_z80dasm_t* win);
+void ui_dasm_discard(ui_dasm_t* win);
 /* open a window */
-void ui_z80dasm_open(ui_z80dasm_t* win);
+void ui_dasm_open(ui_dasm_t* win);
 /* close a window */
-void ui_z80dasm_close(ui_z80dasm_t* win);
+void ui_dasm_close(ui_dasm_t* win);
 /* toggle window visibility */
-void ui_z80dasm_toggle(ui_z80dasm_t* win);
+void ui_dasm_toggle(ui_dasm_t* win);
 /* return true if window is open */
-bool ui_z80dasm_isopen(ui_z80dasm_t* win);
+bool ui_dasm_isopen(ui_dasm_t* win);
 /* draw the window */
-void ui_z80dasm_draw(ui_z80dasm_t* win);
+void ui_dasm_draw(ui_dasm_t* win);
 
 #ifdef __cplusplus
 } /* extern "C" */
@@ -125,10 +125,10 @@ void ui_z80dasm_draw(ui_z80dasm_t* win);
     #define CHIPS_ASSERT(c) assert(c)
 #endif
 
-void ui_z80dasm_init(ui_z80dasm_t* win, ui_z80dasm_desc_t* desc) {
+void ui_dasm_init(ui_dasm_t* win, ui_dasm_desc_t* desc) {
     CHIPS_ASSERT(win && desc);
     CHIPS_ASSERT(desc->title);
-    memset(win, 0, sizeof(ui_z80dasm_t));
+    memset(win, 0, sizeof(ui_dasm_t));
     win->title = desc->title;
     win->read_cb = desc->read_cb;
     win->user_data = desc->user_data;
@@ -136,7 +136,7 @@ void ui_z80dasm_init(ui_z80dasm_t* win, ui_z80dasm_desc_t* desc) {
     win->init_y = desc->y;
     win->init_w = desc->w;
     win->init_h = desc->h;
-    for (int i = 0; i < UI_Z80DASM_MAX_LAYERS; i++) {
+    for (int i = 0; i < UI_DASM_MAX_LAYERS; i++) {
         if (desc->layers[i]) {
             win->num_layers++;
             win->layers[i] = desc->layers[i];
@@ -148,50 +148,50 @@ void ui_z80dasm_init(ui_z80dasm_t* win, ui_z80dasm_desc_t* desc) {
     win->valid = true;
 }
 
-void ui_z80dasm_discard(ui_z80dasm_t* win) {
+void ui_dasm_discard(ui_dasm_t* win) {
     CHIPS_ASSERT(win && win->valid);
     win->valid = false;
 }
 
-void ui_z80dasm_open(ui_z80dasm_t* win) {
+void ui_dasm_open(ui_dasm_t* win) {
     CHIPS_ASSERT(win && win->valid);
     win->open = true;
 }
 
-void ui_z80dasm_close(ui_z80dasm_t* win) {
+void ui_dasm_close(ui_dasm_t* win) {
     CHIPS_ASSERT(win && win->valid);
     win->open = false;
 }
 
-void ui_z80dasm_toggle(ui_z80dasm_t* win) {
+void ui_dasm_toggle(ui_dasm_t* win) {
     CHIPS_ASSERT(win && win->valid);
     win->open = !win->open;
 }
 
-bool ui_z80dasm_isopen(ui_z80dasm_t* win) {
+bool ui_dasm_isopen(ui_dasm_t* win) {
     CHIPS_ASSERT(win && win->valid);
     return win->open;
 }
 
-static uint8_t _ui_z80dasm_in_cb(void* user_data) {
-    ui_z80dasm_t* win = (ui_z80dasm_t*) user_data;
+static uint8_t _ui_dasm_in_cb(void* user_data) {
+    ui_dasm_t* win = (ui_dasm_t*) user_data;
     return win->read_cb(win->cur_layer, win->cur_addr++, win->user_data);
 }
 
-static void _ui_z80dasm_out_cb(char c, void* user_data) {
-    ui_z80dasm_t* win = (ui_z80dasm_t*) user_data;
-    if ((win->str_pos+1) < UI_Z80DASM_MAX_STRLEN) {
+static void _ui_dasm_out_cb(char c, void* user_data) {
+    ui_dasm_t* win = (ui_dasm_t*) user_data;
+    if ((win->str_pos+1) < UI_DASM_MAX_STRLEN) {
         win->str_buf[win->str_pos++] = c;
         win->str_buf[win->str_pos] = 0;
     }
 }
 
-static void _ui_z80dasm_disasm(ui_z80dasm_t* win) {
+static void _ui_dasm_disasm(ui_dasm_t* win) {
     win->str_pos = 0;
-    z80dasm_op(win->cur_addr, _ui_z80dasm_in_cb, _ui_z80dasm_out_cb, win);
+    z80dasm_op(win->cur_addr, _ui_dasm_in_cb, _ui_dasm_out_cb, win);
 }
 
-void ui_z80dasm_draw(ui_z80dasm_t* win) {
+void ui_dasm_draw(ui_dasm_t* win) {
     CHIPS_ASSERT(win && win->valid && win->title);
     if (!win->open) {
         return;
@@ -206,18 +206,18 @@ void ui_z80dasm_draw(ui_z80dasm_t* win) {
         const float line_height = ImGui::GetTextLineHeight();
         const float glyph_width = ImGui::CalcTextSize("F").x;
         const float cell_width = 3 * glyph_width;
-        ImGuiListClipper clipper(UI_Z80DASM_NUM_LINES, line_height);
+        ImGuiListClipper clipper(UI_DASM_NUM_LINES, line_height);
 
         /* skip hidden lines */
         win->cur_addr = win->start_addr;
-        for (int line_i = 0; (line_i < clipper.DisplayStart) && (line_i < UI_Z80DASM_NUM_LINES); line_i++) {
-            _ui_z80dasm_disasm(win);
+        for (int line_i = 0; (line_i < clipper.DisplayStart) && (line_i < UI_DASM_NUM_LINES); line_i++) {
+            _ui_dasm_disasm(win);
         }
 
         /* visible items */
         for (int line_i = clipper.DisplayStart; line_i < clipper.DisplayEnd; line_i++) {
             const int op_addr = (int)win->cur_addr;
-            _ui_z80dasm_disasm(win);
+            _ui_dasm_disasm(win);
             const int num_bytes = (int)win->cur_addr - op_addr;
 
             /* address */
