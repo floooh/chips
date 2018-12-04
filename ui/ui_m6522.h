9c6bd4aba0054f2cacea303bd29a801b2986b569
Andre Weissflog <floooh@gmail.com>
2018-12-04 20:31:12 +0100

    ui_m6522.h: make more similar to ui_m6526.h

--- ui/ui_m6522.h
+++ ui/ui_m6522.h
@@ -59,18 +59,20 @@ extern "C" {
 /* setup parameters for ui_m6522_init()
     NOTE: all string data must remain alive until ui_m6522_discard()!
 */
-typedef struct {
+typedef struct ui_m6522_desc_t {
     const char* title;          /* window title */
     m6522_t* via;               /* m6522_t instance to track */
     int x, y;                   /* initial window pos */
+    int w, h;                   /* initial window size (or 0 for default size) */
     bool open;                  /* initial window open state */
     ui_chip_desc_t chip_desc;   /* chip visualization desc */
 } ui_m6522_desc_t;
 
-typedef struct {
+typedef struct ui_m6522_t {
     const char* title;
     m6522_t* via;
     float init_x, init_y;
+    float init_w, init_h;
     bool open;
     bool valid;
     ui_chip_t chip;
@@ -104,6 +106,8 @@ void ui_m6522_init(ui_m6522_t* win, const ui_m6522_desc_t* desc) {
     win->via = desc->via;
     win->init_x = (float) desc->x;
     win->init_y = (float) desc->y;
+    win->init_w = (float) ((desc->w == 0) ? 420 : desc->w);
+    win->init_h = (float) ((desc->h == 0) ? 380 : desc->h);
     win->open = desc->open;
     win->valid = true;
     ui_chip_init(&win->chip, &desc->chip_desc);
@@ -118,28 +122,28 @@ static void _ui_m6522_draw_state(ui_m6522_t* win) {
     const m6522_t* via = win->via;
     ImGui::Columns(3, "##via_columns", false);
     ImGui::SetColumnWidth(0, 64);
-    ImGui::SetColumnWidth(1, 40);
-    ImGui::SetColumnWidth(2, 40);
+    ImGui::SetColumnWidth(1, 80);
+    ImGui::SetColumnWidth(2, 80);
     ImGui::NextColumn();
-    ImGui::Text("PA"); ImGui::NextColumn();
-    ImGui::Text("PB"); ImGui::NextColumn();
+    ImGui::Text("Port A"); ImGui::NextColumn();
+    ImGui::Text("Port B"); ImGui::NextColumn();
     ImGui::Separator();
     ImGui::Text("DDR"); ImGui::NextColumn();
-    ImGui::Text("%02X", via->ddr_a); ImGui::NextColumn();
-    ImGui::Text("%02X", via->ddr_b); ImGui::NextColumn();
-    ImGui::Text("Out"); ImGui::NextColumn();
-    ImGui::Text("%02X", via->out_a); ImGui::NextColumn();
-    ImGui::Text("%02X", via->out_b); ImGui::NextColumn();
-    ImGui::Text("In"); ImGui::NextColumn();
-    ImGui::Text("%02X", via->in_a); ImGui::NextColumn();
-    ImGui::Text("%02X", via->in_b); ImGui::NextColumn();
-    ImGui::Text("Port"); ImGui::NextColumn();
-    ImGui::Text("%02X", via->port_a); ImGui::NextColumn();
-    ImGui::Text("%02X", via->port_b); ImGui::NextColumn();
+    ui_util_b8("", via->ddr_a); ImGui::NextColumn();
+    ui_util_b8("", via->ddr_b); ImGui::NextColumn();
+    ImGui::Text("Output"); ImGui::NextColumn();
+    ui_util_b8("", via->out_a); ImGui::NextColumn();
+    ui_util_b8("", via->out_b); ImGui::NextColumn();
+    ImGui::Text("Input"); ImGui::NextColumn();
+    ui_util_b8("", via->in_a); ImGui::NextColumn();
+    ui_util_b8("", via->in_b); ImGui::NextColumn();
+    ImGui::Text("Pins"); ImGui::NextColumn();
+    ui_util_b8("", via->port_a); ImGui::NextColumn();
+    ui_util_b8("", via->port_b); ImGui::NextColumn();
     ImGui::Separator();
     ImGui::NextColumn();
-    ImGui::Text("T1"); ImGui::NextColumn();
-    ImGui::Text("T2"); ImGui::NextColumn();
+    ImGui::Text("Timer 1"); ImGui::NextColumn();
+    ImGui::Text("Timer 2"); ImGui::NextColumn();
     ImGui::Separator();
     ImGui::Text("Active"); ImGui::NextColumn();
     ImGui::Text("%s", via->t1_active ? "YES":"NO"); ImGui::NextColumn();
@@ -162,7 +166,7 @@ void ui_m6522_draw(ui_m6522_t* win) {
         return;
     }
     ImGui::SetNextWindowPos(ImVec2(win->init_x, win->init_y), ImGuiSetCond_Once);
-    ImGui::SetNextWindowSize(ImVec2(360, 380), ImGuiSetCond_Once);
+    ImGui::SetNextWindowSize(ImVec2(win->init_w, win->init_h), ImGuiSetCond_Once);
     if (ImGui::Begin(win->title, &win->open)) {
         ImGui::BeginChild("##m6522_chip", ImVec2(176, 0), true);
         ui_chip_draw(&win->chip, win->via->pins);
