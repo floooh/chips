a4c3fa5d4137f2280de643515dea478d9070b3a7
Andre Weissflog <floooh@gmail.com>
2018-12-17 23:20:33 +0100

    ui_zx.h: add ui_dbg.h

--- ui/ui_zx.h
+++ ui/ui_zx.h
@@ -30,6 +30,7 @@
     - ui_audio.h
     - ui_kbd.h
     - ui_dasm.h
+    - ui_dbg.h
     - ui_memedit.h
     - ui_memmap.h
 
@@ -64,6 +65,10 @@ typedef void (*ui_zx_boot_t)(zx_t* sys, zx_type_t type);
 typedef struct {
     zx_t* zx;
     ui_zx_boot_t boot_cb; /* user-provided callback to reboot to different config */
+    ui_dbg_create_texture_t create_texture_cb;      /* texture creation callback for ui_dbg_t */
+    ui_dbg_update_texture_t update_texture_cb;      /* texture update callback for ui_dbg_t */
+    ui_dbg_destroy_texture_t destroy_texture_cb;    /* texture destruction callback for ui_dbg_t */
+    ui_dbg_keydesc_t dbg_keys;          /* user-defined hotkeys for ui_dbg_t */
 } ui_zx_desc_t;
 
 typedef struct {
@@ -76,11 +81,14 @@ typedef struct {
     ui_memmap_t memmap;
     ui_memedit_t memedit[4];
     ui_dasm_t dasm[4];
+    ui_dbg_t dbg;
 } ui_zx_t;
 
 void ui_zx_init(ui_zx_t* ui, const ui_zx_desc_t* desc);
 void ui_zx_discard(ui_zx_t* ui);
 void ui_zx_draw(ui_zx_t* ui, double time_ms);
+bool ui_zx_before_exec(ui_zx_t* ui);
+void ui_zx_after_exec(ui_zx_t* ui);
 
 #ifdef __cplusplus
 } /* extern "C" */
@@ -107,12 +115,15 @@ static void _ui_zx_draw_menu(ui_zx_t* ui, double time_ms) {
         if (ImGui::BeginMenu("System")) {
             if (ImGui::MenuItem("Reset")) {
                 zx_reset(ui->zx);
+                ui_dbg_reset(&ui->dbg);
             }
             if (ImGui::MenuItem("ZX Spectrum 48K", 0, (ui->zx->type == ZX_TYPE_48K))) {
                 ui->boot_cb(ui->zx, ZX_TYPE_48K);
+                ui_dbg_reboot(&ui->dbg);
             }
             if (ImGui::MenuItem("ZX Spectrum 128", 0, (ui->zx->type == ZX_TYPE_128))) {
                 ui->boot_cb(ui->zx, ZX_TYPE_128);
+                ui_dbg_reboot(&ui->dbg);
             }
             if (ImGui::BeginMenu("Joystick")) {
                 if (ImGui::MenuItem("None", 0, (ui->zx->joystick_type == ZX_JOYSTICKTYPE_NONE))) {
@@ -145,6 +156,9 @@ static void _ui_zx_draw_menu(ui_zx_t* ui, double time_ms) {
             ImGui::EndMenu();
         }
         if (ImGui::BeginMenu("Debug")) {
+            ImGui::MenuItem("CPU Debugger", 0, &ui->dbg.ui.open);
+            ImGui::MenuItem("Breakpoints", 0, &ui->dbg.ui.show_breakpoints);
+            ImGui::MenuItem("Memory Heatmap", 0, &ui->dbg.ui.show_heatmap);
             if (ImGui::BeginMenu("Memory Editor")) {
                 ImGui::MenuItem("Window #1", 0, &ui->memedit[0].open);
                 ImGui::MenuItem("Window #2", 0, &ui->memedit[1].open);
@@ -159,7 +173,6 @@ static void _ui_zx_draw_menu(ui_zx_t* ui, double time_ms) {
                 ImGui::MenuItem("Window #4", 0, &ui->dasm[3].open);
                 ImGui::EndMenu();
             }
-            ImGui::MenuItem("CPU Debugger (TODO)");
             ImGui::EndMenu();
         }
         ui_util_options_menu(time_ms, false);
@@ -324,13 +337,28 @@ static const ui_chip_pin_t _ui_zx_ay_pins[] = {
     { "IOA7", 18, AY38910_IOA7 },
 };
 
-void ui_zx_init(ui_zx_t* ui, const ui_zx_desc_t* desc) {
-    CHIPS_ASSERT(ui && desc);
-    CHIPS_ASSERT(desc->zx);
-    CHIPS_ASSERT(desc->boot_cb);
-    ui->zx = desc->zx;
-    ui->boot_cb = desc->boot_cb;
+void ui_zx_init(ui_zx_t* ui, const ui_zx_desc_t* ui_desc) {
+    CHIPS_ASSERT(ui && ui_desc);
+    CHIPS_ASSERT(ui_desc->zx);
+    CHIPS_ASSERT(ui_desc->boot_cb);
+    ui->zx = ui_desc->zx;
+    ui->boot_cb = ui_desc->boot_cb;
     int x = 20, y = 20, dx = 10, dy = 10;
+    {
+        ui_dbg_desc_t desc = {0};
+        desc.title = "CPU Debugger";
+        desc.x = x;
+        desc.y = y;
+        desc.z80 = &ui->zx->cpu;
+        desc.read_cb = _ui_zx_mem_read;
+        desc.create_texture_cb = ui_desc->create_texture_cb;
+        desc.update_texture_cb = ui_desc->update_texture_cb;
+        desc.destroy_texture_cb = ui_desc->destroy_texture_cb;
+        desc.keys = ui_desc->dbg_keys;
+        desc.user_data = ui->zx;
+        ui_dbg_init(&ui->dbg, &desc);
+    }
+    x += dx; y += dy;
     {
         ui_z80_desc_t desc = {0};
         desc.title = "Z80 CPU";
@@ -340,6 +368,7 @@ void ui_zx_init(ui_zx_t* ui, const ui_zx_desc_t* desc) {
         UI_CHIP_INIT_DESC(&desc.chip_desc, "Z80\nCPU", 36, _ui_zx_cpu_pins);
         ui_z80_init(&ui->cpu, &desc);
     }
+    x += dx; y += dy;
     {
         ui_ay38910_desc_t desc = {0};
         desc.title = "AY-3-8912";
@@ -435,6 +464,7 @@ void ui_zx_discard(ui_zx_t* ui) {
         ui_memedit_discard(&ui->memedit[i]);
         ui_dasm_discard(&ui->dasm[i]);
     }
+    ui_dbg_discard(&ui->dbg);
 }
 
 void ui_zx_draw(ui_zx_t* ui, double time_ms) {
@@ -452,7 +482,19 @@ void ui_zx_draw(ui_zx_t* ui, double time_ms) {
         ui_memedit_draw(&ui->memedit[i]);
         ui_dasm_draw(&ui->dasm[i]);
     }
+    ui_dbg_draw(&ui->dbg);
 }
+
+bool ui_zx_before_exec(ui_zx_t* ui) {
+    CHIPS_ASSERT(ui && ui->zx);
+    return ui_dbg_before_exec(&ui->dbg);
+}
+
+void ui_zx_after_exec(ui_zx_t* ui) {
+    CHIPS_ASSERT(ui && ui->zx);
+    ui_dbg_after_exec(&ui->dbg);
+}
+
 #ifdef __clang__
 #pragma clang diagnostic pop
 #endif
