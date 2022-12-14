b575de80800641846ff9ed6980e3f744222855dc
Andre Weissflog <floooh@gmail.com>
2022-12-14 14:30:31 +0100

    mem.h: code cleanup, eliminating signed integers in interface, loop counters and indices

--- chips/mem.h
+++ chips/mem.h
@@ -82,81 +82,6 @@
     - **unmapped page**: the read-pointer points to the internal junk-read-page, and
       the write-pointer to the internal junk-write-page
 
-    ## Functions
-    ~~~C
-    void mem_init(mem_t* mem);
-    ~~~
-    Initialize a new mem_t instance.
-
-    ~~~C
-    void mem_map_ram(mem_t* mem, int layer, uint16_t addr, uint32_t size, uint8_t* ptr)
-    ~~~
-    Map a range of host memory to a 16-bit address for RAM access in a
-    given layer (0..3, 0 being the highest priority layer). Size is in bytes,
-    must be a multiple of 0x0400 (decimal: 1024), and must be <= 0x10000 (decimal: 65536)
-
-    ~~~C
-    void mem_map_rom(mem_t* mem, int layer, uint16_t addr, uint32_t size, const uint8_t* ptr)
-    ~~~
-    Map a range of host memory to a 16-bit address for ROM access.
-    See mem_map_ram() for more details.
-
-    ~~~C
-    void mem_map_rw(mem_t* mem, int layer, uint16_t addr, uint32_t size, const uint8_t* read_ptr, uint8_t* write_ptr)
-    ~~~
-    Map two host memory ranges to a 16-bit address for RAM-behind-ROM access.
-    Read accesses will come from _read_ptr_, and write accesses will go
-    to _write_ptr_. See mem_map_ram() for more details.
-
-    ~~~C
-    void mem_unmap_layer(mem_t* mem, int layer)
-    ~~~
-    Unmap all memory pages in a layer.
-
-    ~~~C
-    void mem_unmap_all(mem_t* mem)
-    ~~~
-    Unmap all memory pages in all layers.
-
-    ~~~C
-    uint8_t mem_rd(mem_t* mem, uint16_t addr)
-    ~~~
-    Read a byte from a 16-bit memory address from the CPU-visible
-    memory page at that location. If the location is unmapped the read will
-    come from the internal read-junk-page and  0xFF will be returned.
-
-    ~~~C
-    void mem_wr(mem_t* mem, uint16_t addr, uint8_t data)
-    ~~~
-    Write a byte to a 16-bit memory address to the CPU-visible memory
-    page at that location. If the location is unmapped or ROM, the write
-    will go the internal write-junk-page.
-
-    ~~~C
-    uint8_t* mem_readptr(mem_t* mem, uint16_t addr)
-    ~~~
-    A helper-function which returns the host-memory location of a 16-bit
-    address for a read-access. Careful, this will return a pointer into the 
-    internal read-junk-page if the page item is unmapped.
-
-    ~~~C
-    void mem_write_range(mem_t* mem, uint16_t addr, const uint8_t* src, int num_bytes)
-    ~~~
-    A helper function to copy a range of bytes from host memory to a 16-bit
-    address range. This will do a series of mem_wr() calls. 
-
-    ~~~C
-    void mem_wr16(mem_t* mem, uint16_t addr, uint16_t data)
-    ~~~
-    A helper function to write a 16-bit value in little-endian format.
-    This will do 2 calls to mem_wr().
-
-    ~~~C
-    uint16_t mem_rd16(mem_t* mem, uint16_t addr)
-    ~~~
-    A helper function to read a 16-bit value in little-endian format.
-    This will do 2 calls to mem_rd().
-
     ## zlib/libpng license
 
     Copyright (c) 2018 Andre Weissflog
@@ -177,22 +102,23 @@
 #*/
 #include <stdint.h>
 #include <stdbool.h>
+#include <stddef.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /* address range (64 KByte) */
-#define MEM_ADDR_RANGE (1<<16)
+#define MEM_ADDR_RANGE (1U<<16)
 #define MEM_ADDR_MASK (MEM_ADDR_RANGE-1)
 
 /* page size (1 KByte) */
-#define MEM_PAGE_SHIFT (10)
-#define MEM_PAGE_SIZE (1<<MEM_PAGE_SHIFT)
+#define MEM_PAGE_SHIFT (10U)
+#define MEM_PAGE_SIZE (1U<<MEM_PAGE_SHIFT)
 #define MEM_PAGE_MASK (MEM_PAGE_SIZE-1)
 
 #define MEM_NUM_PAGES (MEM_ADDR_RANGE / MEM_PAGE_SIZE)
-#define MEM_NUM_LAYERS (4)
+#define MEM_NUM_LAYERS (4U)
 
 /* a memory page item maps a chunk of emulator memory to host memory */
 typedef struct {
@@ -211,19 +137,19 @@ typedef struct {
 /* initialize a new mem instance */
 void mem_init(mem_t* mem);
 /* map a range of RAM */
-void mem_map_ram(mem_t* mem, int layer, uint16_t addr, uint32_t size, uint8_t* ptr);
+void mem_map_ram(mem_t* mem, size_t layer, uint16_t addr, uint32_t size, uint8_t* ptr);
 /* map a range of ROM */
-void mem_map_rom(mem_t* mem, int layer, uint16_t addr, uint32_t size, const uint8_t* ptr);
+void mem_map_rom(mem_t* mem, size_t layer, uint16_t addr, uint32_t size, const uint8_t* ptr);
 /* map a range of memory to different read/write pointers (e.g. for RAM behind ROM) */
-void mem_map_rw(mem_t* mem, int layer, uint16_t addr, uint32_t size, const uint8_t* read_ptr, uint8_t* write_ptr);
+void mem_map_rw(mem_t* mem, size_t layer, uint16_t addr, uint32_t size, const uint8_t* read_ptr, uint8_t* write_ptr);
 /* unmap all memory pages in a layer, also updates the CPU-visible page-table */
-void mem_unmap_layer(mem_t* mem, int layer);
+void mem_unmap_layer(mem_t* mem, size_t layer);
 /* unmap all memory pages in all layers, also updates the CPU-visible page-table */
 void mem_unmap_all(mem_t* mem);
 /* get the host-memory read-ptr of an emulator memory address */
 uint8_t* mem_readptr(mem_t* mem, uint16_t addr);
 /* copy a range of bytes into memory via mem_wr() */
-void mem_write_range(mem_t* mem, uint16_t addr, const uint8_t* src, int num_bytes);
+void mem_write_range(mem_t* mem, uint16_t addr, const uint8_t* src, uint32_t num_bytes);
 
 /* read a byte at 16-bit address */
 static inline uint8_t mem_rd(mem_t* mem, uint16_t addr) {
@@ -246,9 +172,9 @@ static inline uint16_t mem_rd16(mem_t* mem, uint16_t addr) {
 }
 
 /* read a byte from a specific layer (slow!) */
-uint8_t mem_layer_rd(mem_t* mem, int layer, uint16_t addr);
+uint8_t mem_layer_rd(mem_t* mem, size_t layer, uint16_t addr);
 /* write a byte to a specific layer (slow!) */
-void mem_layer_wr(mem_t* mem, int layer, uint16_t addr, uint8_t data);
+void mem_layer_wr(mem_t* mem, size_t layer, uint16_t addr, uint8_t data);
 
 #ifdef __cplusplus
 } /* extern "C" */
@@ -275,9 +201,9 @@ void mem_init(mem_t* m) {
 }
 
 /* this sets the CPU-visible mapping of a page in the page-table */
-static void _mem_update_page_table(mem_t* m, int page_index) {
+static void _mem_update_page_table(mem_t* m, size_t page_index) {
     /* find highest priority layer which maps this memory page */
-    int layer_index;
+    size_t layer_index;
     for (layer_index = 0; layer_index < MEM_NUM_LAYERS; layer_index++) {
         if (m->layers[layer_index][page_index].read_ptr) {
             /* found highest priority layer with valid mapping */
@@ -286,6 +212,10 @@ static void _mem_update_page_table(mem_t* m, int page_index) {
     }
     if (layer_index != MEM_NUM_LAYERS) { 
         /* found a valid mapping */
+
+//        m->page_table[page_index].read_ptr = m->layers[layer_index][page_index].read_ptr;
+//        m->page_table[page_index].write_ptr = m->layers[layer_index][page_index].write_ptr;
+
         m->page_table[page_index] = m->layers[layer_index][page_index];
     }
     else {
@@ -295,17 +225,17 @@ static void _mem_update_page_table(mem_t* m, int page_index) {
     }
 }
 
-static void _mem_map(mem_t* m, int layer, uint16_t addr, uint32_t size, const uint8_t* read_ptr, uint8_t* write_ptr) {
+static void _mem_map(mem_t* m, size_t layer, uint16_t addr, uint32_t size, const uint8_t* read_ptr, uint8_t* write_ptr) {
     CHIPS_ASSERT(m);
     CHIPS_ASSERT((layer >= 0) && (layer < MEM_NUM_LAYERS));
     CHIPS_ASSERT((addr & MEM_PAGE_MASK) == 0);
     CHIPS_ASSERT((size & MEM_PAGE_MASK) == 0);
     CHIPS_ASSERT(size <= MEM_ADDR_RANGE);
-    const int num = size>>MEM_PAGE_SHIFT;
+    const size_t num = size>>MEM_PAGE_SHIFT;
     CHIPS_ASSERT(num <= MEM_NUM_PAGES);
-    for (int i = 0; i < num; i++) {
+    for (size_t i = 0; i < num; i++) {
         const uint16_t offset = i * MEM_PAGE_SIZE;
-        /* the page_index will wrap-around */
+        // the page_index will wrap-around
         const uint16_t page_index = ((addr+offset) & MEM_ADDR_MASK) >> MEM_PAGE_SHIFT;
         CHIPS_ASSERT(page_index <= MEM_NUM_PAGES);
         mem_page_t* page = &m->layers[layer][page_index];
@@ -320,25 +250,25 @@ static void _mem_map(mem_t* m, int layer, uint16_t addr, uint32_t size, const ui
     }
 }
 
-void mem_map_ram(mem_t* m, int layer, uint16_t addr, uint32_t size, uint8_t* ptr) {
+void mem_map_ram(mem_t* m, size_t layer, uint16_t addr, uint32_t size, uint8_t* ptr) {
     CHIPS_ASSERT(ptr);
     _mem_map(m, layer, addr, size, ptr, ptr);
 }
 
-void mem_map_rom(mem_t* m, int layer, uint16_t addr, uint32_t size, const uint8_t* ptr) {
+void mem_map_rom(mem_t* m, size_t layer, uint16_t addr, uint32_t size, const uint8_t* ptr) {
     CHIPS_ASSERT(ptr);
     _mem_map(m, layer, addr, size, ptr, 0);
 }
 
-void mem_map_rw(mem_t* m, int layer, uint16_t addr, uint32_t size, const uint8_t* read_ptr, uint8_t* write_ptr) {
+void mem_map_rw(mem_t* m, size_t layer, uint16_t addr, uint32_t size, const uint8_t* read_ptr, uint8_t* write_ptr) {
     CHIPS_ASSERT(read_ptr && write_ptr);
     _mem_map(m, layer, addr, size, read_ptr, write_ptr);
 }
 
-void mem_unmap_layer(mem_t* m, int layer) {
+void mem_unmap_layer(mem_t* m, size_t layer) {
     CHIPS_ASSERT(m);
     CHIPS_ASSERT((layer >= 0) && (layer < MEM_NUM_LAYERS));
-    for (int page_index = 0; page_index < MEM_NUM_PAGES; page_index++) {
+    for (size_t page_index = 0; page_index < MEM_NUM_PAGES; page_index++) {
         mem_page_t* page = &m->layers[layer][page_index];
         page->read_ptr = 0; 
         page->write_ptr = 0;
@@ -347,14 +277,14 @@ void mem_unmap_layer(mem_t* m, int layer) {
 }
 
 void mem_unmap_all(mem_t* m) {
-    for (int layer_index = 0; layer_index < MEM_NUM_LAYERS; layer_index++) {
-        for (int page_index = 0; page_index < MEM_NUM_PAGES; page_index++) {
+    for (size_t layer_index = 0; layer_index < MEM_NUM_LAYERS; layer_index++) {
+        for (size_t page_index = 0; page_index < MEM_NUM_PAGES; page_index++) {
             mem_page_t* page = &m->layers[layer_index][page_index];
             page->read_ptr = 0; 
             page->write_ptr = 0;
         }
     }
-    for (int page_index = 0; page_index < MEM_NUM_PAGES; page_index++) {
+    for (size_t page_index = 0; page_index < MEM_NUM_PAGES; page_index++) {
         _mem_update_page_table(m, page_index);
     }
 }
@@ -364,13 +294,13 @@ uint8_t* mem_readptr(mem_t* m, uint16_t addr) {
     return (uint8_t*) &(m->page_table[addr>>MEM_PAGE_SHIFT].read_ptr[addr&MEM_PAGE_MASK]);
 } 
 
-void mem_write_range(mem_t* m, uint16_t addr, const uint8_t* src, int num_bytes) {
-    for (int i = 0; i < num_bytes; i++) {
+void mem_write_range(mem_t* m, uint16_t addr, const uint8_t* src, uint32_t num_bytes) {
+    for (size_t i = 0; i < num_bytes; i++) {
         mem_wr(m, addr++, src[i]);
     }
 }
 
-uint8_t mem_layer_rd(mem_t* mem, int layer, uint16_t addr) {
+uint8_t mem_layer_rd(mem_t* mem, size_t layer, uint16_t addr) {
     CHIPS_ASSERT((layer >= 0) && (layer < MEM_NUM_LAYERS));
     if (mem->layers[layer][addr>>MEM_PAGE_SHIFT].read_ptr) {
         return mem->layers[layer][addr>>MEM_PAGE_SHIFT].read_ptr[addr&MEM_PAGE_MASK];
@@ -380,7 +310,7 @@ uint8_t mem_layer_rd(mem_t* mem, int layer, uint16_t addr) {
     }
 }
 
-void mem_layer_wr(mem_t* mem, int layer, uint16_t addr, uint8_t data) {
+void mem_layer_wr(mem_t* mem, size_t layer, uint16_t addr, uint8_t data) {
     CHIPS_ASSERT((layer >= 0) && (layer < MEM_NUM_LAYERS));
     if (mem->layers[layer][addr>>MEM_PAGE_SHIFT].write_ptr) {
         mem->layers[layer][addr>>MEM_PAGE_SHIFT].write_ptr[addr&MEM_PAGE_MASK] = data;
